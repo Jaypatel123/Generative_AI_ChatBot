ae31e8d7d3aaa72e38f05321adf774dc9274cb7e
Jaypatel123 <jaypatel97043@gmail.com>
2024-03-07 05:25:21 -0500

    final commit

--- Chatbot+Code.py
+++ Chatbot+Code.py
@@ -12,7 +12,6 @@ from langchain_community.chat_models import ChatOpenAI
 
 
 load_dotenv()
-# OPENAI_API_KEY = "sk-PHzh89333124kjfd38493jjfdk84035i5Z" #Pass your key here
 OPENAI_API_KEY = os.getenv("API_KEY")
 
 #Upload PDF files
