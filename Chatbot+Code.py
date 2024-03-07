0dad13eecc4875287807e9068a764c351097d39b
Jaypatel123 <jaypatel97043@gmail.com>
2024-03-07 02:37:06 -0500

    adding Virtual Environment

--- Chatbot+Code.py
+++ Chatbot+Code.py
@@ -1,5 +1,13 @@
+import os
 import streamlit as st
 from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+
+
+
+
+# OPENAI_API_KEY = "sk-PHzh89333124kjfd38493jjfdk84035i5Z" #Pass your key here
+OPENAI_API_KEY = os.getenv("API_KEY")
 
 #Upload PDF files
 st.header("My first Chatbot")
